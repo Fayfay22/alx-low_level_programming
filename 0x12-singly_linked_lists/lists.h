5e29a91decf9bd89f2a2b43f5a1f6ab1166e28d3
Fayfay22 <faithokoko014@gmail.com>
2023-03-30 18:57:31 -0700

    linkedlist

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,27 +1,19 @@
-#ifndef LISTS_H
-#define LISTS_H
+#include "lists.h"
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
  */
-typedef struct list_s
+
+void free_list(list_t *head)
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	list_t *current;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
-#endif
-
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
